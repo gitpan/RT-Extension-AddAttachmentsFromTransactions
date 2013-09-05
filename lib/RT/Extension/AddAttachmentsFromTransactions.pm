2f74e3a8567f728c981c72b5071eb208e392b9c5
Christian Loos <cloos@cpan.org>
2013-09-05 14:11:15 +0000

    Import of CLOOS/RT-Extension-AddAttachmentsFromTransactions-1.00 from CPAN.
    gitpan-cpan-distribution: RT-Extension-AddAttachmentsFromTransactions
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         CLOOS/RT-Extension-AddAttachmentsFromTransactions-1.00.tar.gz
    gitpan-cpan-author:       CLOOS
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/AddAttachmentsFromTransactions.pm
+++ lib/RT/Extension/AddAttachmentsFromTransactions.pm
@@ -3,70 +3,77 @@ package RT::Extension::AddAttachmentsFromTransactions;
 use 5.008003;
 use strict;
 use warnings;
-no warnings 'redefine';
 
-our $VERSION = '0.01';
+our $VERSION = '1.00';
 
-use RT::Ticket;
-my $orig_note = RT::Ticket->can('_RecordNote');
-*RT::Ticket::_RecordNote = sub {
-    my $self = shift;
-    my %args = @_;
+{
+    require RT::Ticket;
+    my $orig = RT::Ticket->can('_RecordNote');
+    no warnings 'redefine';
+    *RT::Ticket::_RecordNote = sub {
+        my $self = shift;
+        my %args = @_;
 
-    # We can't do anything if we don't have an MIMEObj
-    # so let the original method handle it
-    return $orig_note->($self, %args) unless $args{'MIMEObj'};
+        # We can't do anything if we don't have an MIMEObj
+        # so let the original method handle it
+        return $orig->($self, %args) unless $args{'MIMEObj'};
 
-    # move the Attachment id's from session to the RT-Attach header
-    for my $id ( @{ $HTML::Mason::Commands::session{'AttachExisting'} } ) {
-        $args{'MIMEObj'}->head->add( 'RT-Attach' => $id );
-    }
-
-    # cleanup session
-    delete $HTML::Mason::Commands::session{'AttachExisting'};
-
-    return $orig_note->($self, %args);
-};
-
-use RT::Action::SendEmail;
-my $orig_add_attach = RT::Action::SendEmail->can('AddAttachments');
-*RT::Action::SendEmail::AddAttachments = sub {
-    my $self = shift;
-
-    $orig_add_attach->($self, @_);
-
-    $self->AddAttachmentsFromHeaders();
-};
-
-package RT::Action::SendEmail;
-
-sub AddAttachmentsFromHeaders {
-    my $self  = shift;
-    my $orig  = $self->TransactionObj->Attachments->First;
-    my $email = $self->TemplateObj->MIMEObj;
-
-    use List::MoreUtils qw(uniq);
-
-    # Add the RT-Attach headers from the transaction to the email
-    if ($orig and $orig->GetHeader('RT-Attach')) {
-        for my $id ($orig->ContentAsMIME(Children => 0)->head->get_all('RT-Attach')) {
-            $email->head->add('RT-Attach' => $id);
+        # move the Attachment id's from session to the X-RT-Attach header
+        for my $id ( @{ $HTML::Mason::Commands::session{'AttachExisting'} } ) {
+            $args{'MIMEObj'}->head->add( 'X-RT-Attach' => $id );
         }
-    }
 
-    # Take all RT-Attach headers and add the attachments to the outgoing mail
-    for my $id (uniq $email->head->get_all('RT-Attach')) {
-        $id =~ s/(?:^\s*|\s*$)//g;
+        # cleanup session
+        delete $HTML::Mason::Commands::session{'AttachExisting'};
 
-        my $attach = RT::Attachment->new( $self->TransactionObj->CreatorObj );
-        $attach->Load($id);
-        next unless $attach->Id
+        return $orig->($self, %args);
+    };
+}
+
+{
+    require RT::Action::SendEmail;
+    my $orig = RT::Action::SendEmail->can('AddAttachments');
+    no warnings 'redefine';
+    *RT::Action::SendEmail::AddAttachments = sub {
+        my $self = shift;
+
+        $orig->($self, @_);
+
+        $self->AddAttachmentsFromHeaders();
+    };
+}
+
+{
+    package RT::Action::SendEmail;
+
+    sub AddAttachmentsFromHeaders {
+        my $self  = shift;
+        my $orig  = $self->TransactionObj->Attachments->First;
+        my $email = $self->TemplateObj->MIMEObj;
+
+        use List::MoreUtils qw(uniq);
+
+        # Add the X-RT-Attach headers from the transaction to the email
+        if ($orig and $orig->GetHeader('X-RT-Attach')) {
+            for my $id ($orig->ContentAsMIME(Children => 0)->head->get_all('X-RT-Attach')) {
+                $email->head->add('X-RT-Attach' => $id);
+            }
+        }
+
+        # Take all X-RT-Attach headers and add the attachments to the outgoing mail
+        for my $id (uniq $email->head->get_all('X-RT-Attach')) {
+            $id =~ s/(?:^\s*|\s*$)//g;
+
+            my $attach = RT::Attachment->new( $self->TransactionObj->CreatorObj );
+            $attach->Load($id);
+            next unless $attach->Id
                 and $attach->TransactionObj->CurrentUserCanSee;
 
-        if ( !$email->is_multipart ) {
-            $email->make_multipart( 'mixed', Force => 1 );
+            if ( !$email->is_multipart ) {
+                $email->make_multipart( 'mixed', Force => 1 );
+            }
+            $self->AddAttachment($attach, $email);
         }
-        $self->AddAttachment($attach, $email);
     }
 }
 
@@ -126,7 +133,7 @@ it under the same terms as Perl itself.
 
 =item L<http://www.gossamer-threads.com/lists/rt/users/107976>
 
-=item L<https://github.com/bestpractical/rt/tree/4.2/attach-from-transactions>
+=item L<https://github.com/bestpractical/rt/tree/4.4/attach-from-transactions>
 
 =back
 
@@ -135,7 +142,7 @@ it under the same terms as Perl itself.
 Thanks to BÁLINT Bekény for contributing the code from his implementation.
 
 Also Thanks to Best Practical Solutions who are working on this feature for
-RT 4.2 on the '4.2/attach-from-transactions' branch where I've borrowed some
+RT 4.4 on the '4.4/attach-from-transactions' branch where I've borrowed some
 code for this extension.
 
 =cut
